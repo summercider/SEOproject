727abc8f0c50b5f96d2dac4042ff105657ee41c2
summercider <wlstj697@gmail.com>
2025-04-24 15:21:55 +0900

    Coupon.jsx  경로 수정

--- src/components/home/Coupon.jsx
+++ src/components/home/Coupon.jsx
@@ -1,4 +1,4 @@
-import CouponBanner from '@/components/home/couponBanner';
+import CouponBanner from '@/components/home/CouponBanner'; //여기 경로오류에요 대문자구분
 import CouponMore from '@/components/home/CouponMore';
 
 export default function Coupon() {
